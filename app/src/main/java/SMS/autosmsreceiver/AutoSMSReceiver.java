8b67abea0d8d0831b95987ffc421f6884b186b9d
LegalUserName <e.fedorov@gpsonline.com.ua>
2021-08-17 15:07:37 +0300

    first commit

---
+++ app/src/main/java/SMS/autosmsreceiver/AutoSMSReceiver.java
@@ -0,0 +1,47 @@
+package SMS.autosmsreceiver;
+
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import com.google.android.gms.auth.api.phone.SmsRetriever;
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.common.api.Status;
+
+
+public class AutoSMSReceiver extends BroadcastReceiver {
+
+    public String LogTag = "AutoSMSReceiver";
+
+    public interface OnReceiverSMSP{ void onReceiverSMSP(String code);}
+    static public OnReceiverSMSP onReceiverSMSP;
+    static public void Register_onReceiverSMSP(OnReceiverSMSP onReceiverSMSP){ AutoSMSReceiver.onReceiverSMSP = onReceiverSMSP; }
+
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.v(LogTag,String.valueOf(intent.getAction()));
+
+        if (SmsRetriever.SMS_RETRIEVED_ACTION.equals(intent.getAction())) {
+            Bundle extras = intent.getExtras();
+            Status status = (Status) extras.get(SmsRetriever.EXTRA_STATUS);
+
+            switch(status.getStatusCode()) {
+                case CommonStatusCodes.SUCCESS:
+                    String message = (String) extras.get(SmsRetriever.EXTRA_SMS_MESSAGE);
+                    String[] words = message.split(" ");
+                    try {
+                        Log.v(LogTag, "Receive:SUCCESS,Digits:" + words[2]);
+                        onReceiverSMSP.onReceiverSMSP(words[2]);
+                    } catch (NullPointerException e) {
+                        Log.v(LogTag, e.getMessage().toString());
+                    }
+                    break;
+                case CommonStatusCodes.TIMEOUT:
+                    break;
+            }
+        }
+    }
+}
